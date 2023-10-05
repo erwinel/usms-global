28261899a9b36987486385cf70da820f8ae7f384
erwinel <37079914+erwinel@users.noreply.github.com>
2023-10-05 19:16:43 +0000

    Converted to top-level statements

--- util/src/SnTsTypeGenerator/Program.cs
+++ util/src/SnTsTypeGenerator/Program.cs
@@ -6,40 +6,34 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SnTsTypeGenerator;
 
-internal class Program
-{
-    private static IHost _host = null!;
-    private static void Main(string[] args)
+IHost _host = null!;
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+AppSettings.Configure(args, builder.Configuration);
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+var env = builder.Environment;
+if (env.IsDevelopment())
+    builder.Configuration.AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly(), true);
+builder.Logging.AddConsole();
+builder.Services
+    .Configure<AppSettings>(builder.Configuration.GetSection(nameof(SnTsTypeGenerator)))
+    .AddDbContextPool<TypingsDbContext>(options =>
     {
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
-        AppSettings.Configure(args, builder.Configuration);
-        builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
-        var env = builder.Environment;
-        if (env.IsDevelopment())
-            builder.Configuration.AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly(), true);
-        builder.Logging.AddConsole();
-        builder.Services
-            .Configure<AppSettings>(builder.Configuration.GetSection(nameof(SnTsTypeGenerator)))
-            .AddDbContextPool<TypingsDbContext>(options =>
-            {
-                var dbFile = builder.Configuration.Get<AppSettings>()?.DbFile;
-                if (string.IsNullOrEmpty(dbFile))
-                    dbFile = Constants.DEFAULT_DbFile;
-                try { dbFile = new FileInfo((Path.IsPathFullyQualified(dbFile) || Path.IsPathRooted(dbFile)) ? dbFile : Path.Combine(builder.Environment.ContentRootPath, dbFile)).FullName; }
-                catch { }
-                options.UseSqlite(new SqliteConnectionStringBuilder
-                {
-                    DataSource = dbFile,
-                    ForeignKeys = true,
-                    Mode = SqliteOpenMode.ReadWrite
-                }.ConnectionString);
-            })
-            .AddHostedService<MainWorkerService>()
-            .AddTransient<SnClientHandlerService>()
-            .AddTransient<TableAPIService>()
-            .AddTransient<DataLoaderService>()
-            .AddTransient<RenderingService>();
-        _host = builder.Build();
-        _host.Run();
-    }
-}
+        var dbFile = builder.Configuration.Get<AppSettings>()?.DbFile;
+        if (string.IsNullOrEmpty(dbFile))
+            dbFile = Constants.DEFAULT_DbFile;
+        try { dbFile = new FileInfo((Path.IsPathFullyQualified(dbFile) || Path.IsPathRooted(dbFile)) ? dbFile : Path.Combine(builder.Environment.ContentRootPath, dbFile)).FullName; }
+        catch { }
+        options.UseSqlite(new SqliteConnectionStringBuilder
+        {
+            DataSource = dbFile,
+            ForeignKeys = true,
+            Mode = SqliteOpenMode.ReadWrite
+        }.ConnectionString);
+    })
+    .AddHostedService<MainWorkerService>()
+    .AddTransient<SnClientHandlerService>()
+    .AddTransient<TableAPIService>()
+    .AddTransient<DataLoaderService>()
+    .AddTransient<RenderingService>();
+_host = builder.Build();
+_host.Run();
