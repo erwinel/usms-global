142c86e1aadb7dada9ad563f16cd98d3c751e491
Leonard <lenny@erwinefamily.net>
2023-10-19 00:35:31 -0400

    Added deserialization method

--- util/src/SnTsTypeGenerator/Models/TableAPI/RecordRef.cs
+++ util/src/SnTsTypeGenerator/Models/TableAPI/RecordRef.cs
@@ -1,3 +1,7 @@
+using System.Text.Json.Nodes;
+using SnTsTypeGenerator.Services;
+using static SnTsTypeGenerator.Services.SnApiConstants;
+
 namespace SnTsTypeGenerator.Models.TableAPI;
 
 /// <summary>
@@ -5,4 +9,8 @@ namespace SnTsTypeGenerator.Models.TableAPI;
 /// </summary>
 /// <param name="Value">The value of the <c>value</c> property.</param>
 /// <param name="DisplayValue">The value of the <c>display_value</c> property.</param>
-public record RecordRef(string Value, string DisplayValue);
+public record RecordRef(string Value, string DisplayValue)
+{
+    internal static RecordRef? DeserializeProperty(JsonObject obj, string propertyName) => (obj.TryGetProperty(propertyName, out JsonObject? p) && p.TryGetPropertyAsNonEmpty(JSON_KEY_VALUE, out string? value)) ?
+        new(Value: value, DisplayValue: p.CoercePropertyAsNonEmpty(JSON_KEY_DISPLAY_VALUE, value)) : null;
+}
