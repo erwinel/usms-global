a03ab1d6b0eb2b9cc9bcef79cdbe45bb1a54d134
Leonard <lenny@erwinefamily.net>
2023-10-27 23:38:18 -0400

    Added media type check

--- util/src/SnTsTypeGenerator/Services/SnClientHandlerService.cs
+++ util/src/SnTsTypeGenerator/Services/SnClientHandlerService.cs
@@ -49,9 +49,14 @@ public sealed class SnClientHandlerService
         string responseBody;
         try { responseBody = await response.Content.ReadAsStringAsync(cancellationToken); }
         //codeql[cs/catch-of-all-exceptions] Won't fix - API documentation doesn't indicate what type of exception may be thrown.
-        catch (Exception exception) { throw new GetResponseContentFailedException(requestUri, exception); }
+        catch (Exception exception)
+        {
+            throw new GetResponseContentFailedException(requestUri, exception);
+        }
         cancellationToken.ThrowIfCancellationRequested();
-        if (string.IsNullOrWhiteSpace(responseBody)) { throw new InvalidHttpResponseException(requestUri, responseBody); }
+        string? mediaType;
+        if (string.IsNullOrWhiteSpace(responseBody) || (!string.IsNullOrEmpty(mediaType = response.Content.Headers.ContentType?.MediaType) && mediaType != MediaTypeNames.Application.Json))
+            throw new InvalidHttpResponseException(requestUri, responseBody);
         JsonNode? result;
         try { result = JsonNode.Parse(responseBody); }
         catch (JsonException exception) { throw new ResponseParsingException(requestUri, responseBody, exception); }
